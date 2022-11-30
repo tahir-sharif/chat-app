6a119c1516c8bae9d90d77cb9b50dc9fc670c56d
tahir150 <tahirtv.islam@gmail.com>
2022-11-30 13:25:52 +0500

    updated url JS

--- src/url.js
+++ src/url.js
@@ -1,5 +1,6 @@
 // export const apiUrl = (url = "") => "http://localhost:6500/api" + url;
 // export const apiUrl = (url = "") => "http://192.168.50.77:6500/api" + url;
-export const apiUrl = (url = "") => "http://192.168.50.64:6500/api" + url;
-// export const apiUrl = (url = "") =>
-//   "https://chat-server-inky.vercel.app/api" + url; // old server
+// export const apiUrl = (url = "") => "http://192.168.50.64:6500/api" + url;
+// // export const apiUrl = (url = "") =>
+// //   "https://chat-server-inky.vercel.app/api" + url; // old server
+export const apiUrl = (url = "") => "https://chat-server-ten.vercel.app/" + url;
