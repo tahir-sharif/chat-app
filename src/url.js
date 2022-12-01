f5c4a00919ac3bebe4210c4e16ef68d8270de8d4
tahir150 <tahirtv.islam@gmail.com>
2022-12-01 17:05:27 +0500

    url added

--- src/url.js
+++ src/url.js
@@ -1,6 +1,7 @@
 export const apiUrl = (url = "") => {
-  const _DEPLOYED_URL = "https://chat-server-l9wsph65l-tahir150.vercel.app/api";
-  const _LOCAL_URL = "http://localhost:6500/api";
+  const _DEPLOYED_URL =
+    "https://chat-server-l9wsph65l-tahir150.vercel.app/api" + url;
+  const _LOCAL_URL = "http://localhost:6500/api" + url;
 
   const enviromentsURL = {
     production: _DEPLOYED_URL,
