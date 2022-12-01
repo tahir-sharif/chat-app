3bd46c11192939bac741013b4246193bcd32ab09
tahir150 <tahirtv.islam@gmail.com>
2022-12-01 17:02:45 +0500

    enviroments files added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import Routes from "./Routes";
 import "./App.css";
 import UserAccess from "./access-control/UserAccess";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    console.log("Application started on", process.env.NODE_ENV, "mode");
+  }, []);
+
   return (
     <div className="App">
       <UserAccess>
--- src/url.js
+++ src/url.js
@@ -1,8 +1,11 @@
-// export const apiUrl = (url = "") => "http://localhost:6500/api" + url;
-// export const apiUrl = (url = "") => "http://192.168.50.77:6500/api" + url;
-// export const apiUrl = (url = "") => "http://192.168.50.64:6500/api" + url;
-// // export const apiUrl = (url = "") =>
-// //   "https://chat-server-inky.vercel.app/api" + url; // old server
-// export const apiUrl = (url = "") => "https://chat-server-iota.vercel.app/api" + url;
-export const apiUrl = (url = "") => "https://chat-server-l9wsph65l-tahir150.vercel.app/api" + url;
-// export const apiUrl = (url = "") => "https://dead-plum-millipede-robe.cyclic.app/" + url;
+export const apiUrl = (url = "") => {
+  const _DEPLOYED_URL = "https://chat-server-l9wsph65l-tahir150.vercel.app/api";
+  const _LOCAL_URL = "http://localhost:6500/api";
+
+  const enviromentsURL = {
+    production: _DEPLOYED_URL,
+    development: _LOCAL_URL,
+  };
+
+  return enviromentsURL[process.env.NODE_ENV] || _LOCAL_URL;
+};
