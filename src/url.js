d238b6724a838a5fbdb647d9cd1d4169926bf3fe
tahir-shareef <tahir.shareef@bssuniversal.com>
2023-05-08 10:59:12 +0500

    updated server url

--- src/url.js
+++ src/url.js
@@ -1,14 +1,14 @@
-export const apiUrl = (url = "") => {
-  const isForSocket = url && typeof url === "boolean";
-  const subURL = isForSocket ? "" : "/api" + url;
+export const apiUrl = (url = '') => {
+  const isForSocket = url && typeof url === 'boolean';
+  const subURL = isForSocket ? '' : '/api' + url;
 
   const _DEPLOYED_URL =
-    "https://chat-server-l9wsph65l-tahir150.vercel.app" + subURL;
-  const _LOCAL_URL = "http://localhost:6500" + subURL;
+    'https://chat-server-git-master-tahir150.vercel.app' + subURL;
+  const _LOCAL_URL = 'http://localhost:6500' + subURL;
 
   const enviromentsURL = {
     production: _DEPLOYED_URL,
-    development: _LOCAL_URL,
+    development: _LOCAL_URL
   };
 
   const _CONNECTION_URL = enviromentsURL[process.env.NODE_ENV] || _DEPLOYED_URL;
